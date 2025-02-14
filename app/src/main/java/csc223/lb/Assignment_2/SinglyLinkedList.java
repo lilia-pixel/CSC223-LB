a2fcc56ce4d19763dc4b4a330e8d7497d298d3ff
unknown <bellahcenlilia@gmail.com>
2025-02-14 14:10:25 -0500

    lab 3

---
+++ app/src/main/java/csc223/lb/Assignment_2/SinglyLinkedList.java
@@ -0,0 +1,174 @@
+package csc223.lb.Assignment_2;
+
+public class SinglyLinkedList implements LinkedList{
+    public Node head;
+    public Node tail;
+    public int size;
+
+
+    public SinglyLinkedList(){
+        this.head = new Node(null, '\0', null);
+        this.size = 0;
+    }
+     // Insert an item at the end of the list for unsorted lists
+    public void insert(char item){
+        Node newNode = new Node(null, item, null);
+        Node curr = this.head;
+
+        while (curr.next!=null){
+            curr = curr.next;
+        }
+        curr.next = newNode;
+        newNode.next = null;
+        this.size++;
+    }
+
+     // Removes an item from the list
+    public void remove(char item){
+        Node curr = this.head;
+
+        while (curr.next.data != item){
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        this.size --;
+    }
+ 
+     // Get the first item, return '☠' if the list is empty
+    public char getFirst(){
+        if (isEmpty()){
+            return '☠';
+        }
+        return this.head.next.data;
+    }
+ 
+     // Get the last item, return '☠' if the list is empty
+    public char getLast(){
+        if (isEmpty()){
+            return '☠';
+        }
+        Node curr = this.head;
+        while (curr.next != null){
+            curr = curr.next;
+        }
+        return curr.data;
+    }
+ 
+     // Get an item at a specific index, return '☠' if index is out of bounds
+    public char get(int index){
+        if (index<0 || index > size()-1){
+            return '☠';
+        }
+        int i = 0;
+        Node curr = this.head.next;
+        while (i!=index){
+            curr=curr.next;
+            i++;
+        }
+        return curr.data;
+    }
+ 
+     // Get the size of the list
+    public int size(){
+        return this.size;
+    }
+ 
+     // Check if the list is empty
+    public boolean isEmpty(){
+        return this.head.next==null;
+    }
+ 
+     // Clear the list
+    public void clear(){
+        this.size=0;
+        this.head.next = null;
+    }
+ 
+     // Check if the list contains an item
+    public boolean contains(char item){
+        Node curr = this.head.next;
+        while (curr != null){
+            if (curr.data == item){
+                return true;
+            }
+            curr= curr.next;
+        }
+        return false;
+
+    }
+ 
+     // Get the index of an item, if it exists else return -1
+    public int indexOf(char item){
+        if (!contains(item)){
+            return -1;
+        }
+        int i = 0;
+        Node curr = this.head.next;
+        while (curr.data!=item){
+            curr=curr.next;
+            i++;
+        }
+        return i;
+    }
+ 
+     // Get the last index of an item, if it exists else return -1
+    public int lastIndexOf(char item){
+        if (!contains(item)){
+            return -1;
+        }
+        Node curr = this.head;
+        int i = 0;
+        int indx = 0;
+        while (curr.next != null){
+            if (curr.next.data == item){
+                indx=i;
+            }
+            curr = curr.next;
+            i++;
+        }
+        return indx;
+    }
+ 
+     // Reverse the list
+    public void reverse(){
+        Node prev = null;
+        Node curr = this.head.next;
+        Node next = null;
+
+        while (curr != null) {  
+            next = curr.next;
+            curr.next =prev; 
+            prev = curr;
+            curr = next;
+
+        }
+  
+        this.head.next = prev; 
+    }
+ 
+     // Creates and returns a String representation of the list
+     // e.g. `A -> B -> C -> D` should return "ABCD"
+    public String toString(){
+        String s = "";
+        Node curr = this.head.next; 
+        while (curr != this.tail) { 
+            s +=curr.data;
+            curr = curr.next;
+        }
+        return s;
+    }
+
+    public static void main(String[] arg){
+                SinglyLinkedList Sll = new SinglyLinkedList();
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        System.out.println(Sll.toString());
+
+        Sll.reverse();
+        System.out.println(Sll.toString());
+
+        
+    }
+}
---
+++ app/src/test/java/csc223/lb/Assignment_2Test/SinglyLinkedListTest.java
@@ -0,0 +1,197 @@
+package csc223.lb.Assignment_2Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+import csc223.lb.Assignment_2.SinglyLinkedList;
+
+public class SinglyLinkedListTest {
+    
+    @Test
+    public void testInsert(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        assert(Sll.head.next.data == 'A');
+        assert(Sll.head.next.next.data == 'B');
+        assert(Sll.head.next.next.next.data != 'D');
+
+    }
+
+    @Test
+    public void testRemove(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+        assertEquals("ABCDE", Sll.toString());
+
+        Sll.remove('E');
+
+        assertEquals("ABCD", Sll.toString());
+        Sll.remove('A');
+        Sll.remove('B');
+        Sll.remove('C');
+        Sll.remove('D');
+
+        assertTrue(Sll.isEmpty());
+        // assertThrows(Exception.class,() ->{Dll.remove('V');} );
+    }
+
+    @Test
+    public void getFirstTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        assertEquals('☠', Sll.getFirst());
+
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+
+        assertEquals('A', Sll.getFirst());
+
+        Sll.remove('A');
+
+        assertEquals('B', Sll.getFirst());
+    }
+
+    @Test
+    public void getLastTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        assertEquals('☠', Sll.getLast());
+
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+
+        assertEquals('E', Sll.getLast());
+
+        Sll.remove('E');
+
+        assertEquals('D', Sll.getLast());
+
+    }
+
+    @Test
+    public void getTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+
+        assertEquals('B', Sll.get(1));
+        assertEquals('☠', Sll.get(5));
+    }
+
+    @Test
+    public void sizeTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        assertEquals(0, Sll.size());
+
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+        
+        assertEquals(5, Sll.size());
+    }
+
+    @Test
+    public void clearTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+
+        assertEquals(5, Sll.size());
+        Sll.clear();
+        assertEquals(0, Sll.size());
+        assertTrue(Sll.isEmpty());
+
+    }
+
+    @Test
+    public void containsTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+        assertTrue(Sll.contains('D'));
+
+        assertEquals(5, Sll.size());
+        Sll.clear();
+        assertEquals(0, Sll.size());
+        assertFalse(Sll.contains('A'));
+        assertFalse(Sll.contains('B'));
+        assertFalse(Sll.contains('C'));
+        assertFalse(Sll.contains('D'));
+
+        Sll.insert('T');
+        Sll.insert('O');
+        assertTrue(Sll.contains('T'));
+        assertTrue(Sll.contains('O'));
+    }
+
+    @Test
+    public void indexOfTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        assertEquals(-1, Sll.indexOf('a'));
+
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+        assertEquals(2, Sll.indexOf('C'));
+        assertEquals(-1, Sll.indexOf('T'));
+    }
+
+    @Test
+    public void LastIndexOfTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        assertEquals(-1, Sll.indexOf('a'));
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+
+        assertEquals(5, Sll.lastIndexOf('A'));
+        assertEquals(9, Sll.lastIndexOf('E'));
+    }
+
+    @Test
+    public void reverseTest(){
+        SinglyLinkedList Sll = new SinglyLinkedList();
+        Sll.insert('A');
+        Sll.insert('B');
+        Sll.insert('C');
+        Sll.insert('D');
+        Sll.insert('E');
+        assertEquals("ABCDE", Sll.toString());
+        Sll.reverse();
+        assertEquals("EDCBA", Sll.toString());
+        
+    }
+}
