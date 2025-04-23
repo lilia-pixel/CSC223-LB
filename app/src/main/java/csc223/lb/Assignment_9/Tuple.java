f05e02bc8960b57a21d75c4b63ce4d949450af91
ajodell <ajodell108@gmail.com>
2025-04-23 15:27:17 -0400

    Tuple datastructure added

---
+++ app/src/main/java/csc223/lb/Assignment_9/Tuple.java
@@ -0,0 +1,12 @@
+package csc223.lb.Assignment_9;
+
+public class Tuple {
+    
+    String key;
+    Integer value;
+
+    public Tuple(String key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+}
